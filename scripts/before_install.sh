b5c0725cb14d83eb6684715249880f65279aaf69
Joel-Revington <joel.r@usam.in>
2024-08-03 16:04:34 +0530

    build fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+# Remove existing package-lock.json if it exists
+if [ -f /home/ubuntu/package-lock.json ]; then
+  echo "Removing existing package-lock.json"
+  rm /home/ubuntu/package-lock.json
+fi
