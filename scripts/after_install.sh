70797511bf750547a524c41f6981efc7764ba357
Joel-Revington <joel.r@usam.in>
2024-08-03 14:39:14 +0530

    updated yml and sh files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-# Pull the latest Docker image from ECR
-docker pull 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Stop and remove the existing container
+docker stop acc_backup || true
+docker rm acc_backup || true
+
+# Remove existing Docker image
+docker rmi 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest || true
+
+#!/bin/bash
+# Pull the latest Docker image from ECR
+docker pull 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+
+#!/bin/bash
 # Run the Docker container
 docker run -d --name acc_backup -p 80:80 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
 
