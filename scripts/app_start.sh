cf967bbd4f04abd02597aba5869909baa58c185f
JoelRevington1305 <joelrevington28@gmail.com>
2024-08-13 01:26:36 +0530

    removed docker compose

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Stop and remove the existing container
+docker stop acc_backup || true
+docker rm acc_backup || true
 
-# Navigate to the project directory
-cd /home/ubuntu/ACC_Backup
+# Remove existing Docker image
+docker rmi 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest || true
 
-# Bring down any existing containers
-docker-compose down || true
+#!/bin/bash
+# Pull the latest Docker image from ECR
+docker pull 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
 
-docker-compose pull
-
-# Pull the latest image and start the container
-docker-compose up -d
+#!/bin/bash
+# Run the Docker container
+docker run -d --name acc_backup -p 80:80 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
 
 # Restart NGINX to apply any new configuration
 systemctl restart nginx
