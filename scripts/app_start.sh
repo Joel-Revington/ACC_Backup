978321998fdc8839926d0592071d949ec2c0bb9a
JoelRevington1305 <joelrevington28@gmail.com>
2024-08-13 01:59:45 +0530

    check

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,18 +1,35 @@
-#!/bin/bash
-# Stop and remove the existing container
-docker stop acc_backup || true
-docker rm acc_backup || true
+# #!/bin/bash
+# # Stop and remove the existing container
+# docker stop acc_backup || true
+# docker rm acc_backup || true
 
-# Remove existing Docker image
-docker rmi 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest || true
+# # Remove existing Docker image
+# docker rmi 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest || true
+
+# #!/bin/bash
+# # Pull the latest Docker image from ECR
+# docker pull 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+
+# #!/bin/bash
+# # Run the Docker container
+# docker run -d --name acc_backup -p 80:80 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+
+# # Restart NGINX to apply any new configuration
+# systemctl restart nginx
 
 #!/bin/bash
+
+# Navigate to the project directory
+cd /home/ubuntu/ACC_Backup1
+
+# Stop and remove the existing containers if any
+docker-compose down
+
 # Pull the latest Docker image from ECR
-docker pull 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+docker-compose pull
 
-#!/bin/bash
-# Run the Docker container
-docker run -d --name acc_backup -p 80:80 339713031143.dkr.ecr.ap-south-1.amazonaws.com/acc_backup:latest
+# Start the Docker container using Docker Compose with an external .env file
+docker-compose --env-file .env up -d
 
 # Restart NGINX to apply any new configuration
 systemctl restart nginx
