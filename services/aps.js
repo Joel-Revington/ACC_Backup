d1e15265e9b4f0644d621ada68b65a1c282a88eb
JoelRevington1305 <joelrevington28@gmail.com>
2024-08-16 15:46:27 +0530

    log check1

--- services/aps.js
+++ services/aps.js
@@ -237,17 +237,17 @@ service.backupData = async (accessToken) => {
 async function zipDirectory(source, out) {
     const archive = archiver('zip', { zlib: { level: 9 }});
     const stream = fs.createWriteStream(out);
-    console.log(archive, stream);
-
+    
     return new Promise((resolve, reject) => {
         archive
-            .directory(source, false)
-            .on('error', err => reject(err))
-            .pipe(stream)
+        .directory(source, false)
+        .on('error', err => reject(err))
+        .pipe(stream)
         ;
-
+        
         stream.on('close', () => resolve());
         archive.finalize();
+        console.log(archive.finalize());
     });
 }
 
